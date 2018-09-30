c6a7dd398caea157ce118b37d798d60a0691779c
Janusz Sołtysiak <jsoltysiak@gmail.com>
2018-10-01 01:20:16 +0200

    :sparkles: Add script for managing camera aspect ratio

---
+++ Assets/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public float MinimumViewSize = 6.0f;
+
+	private void Start () {
+		var aspectRatio = (float)Screen.width / (float)Screen.height;
+		var cameraHeight = MinimumViewSize / aspectRatio;
+
+		cameraHeight = cameraHeight < MinimumViewSize ? MinimumViewSize : cameraHeight;
+		
+		GetComponent<Camera>().orthographicSize = cameraHeight / 2.0f;
+	}
+}
